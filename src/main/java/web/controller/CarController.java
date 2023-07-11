a8a862b5c5a78f35fcd16165b3ff1b442193306b
Evgeny <pet9.wawa.95@gmail.com>
2023-07-11 21:52:19 +0300

    Done_remarks

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,37 +1,28 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.models.Car;
+import web.service.CarService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
-    private List<Car> carList;
+    private final CarService carService;
 
-    public CarController() {
-        carList = new ArrayList<>();
-        carList.add(new Car("Марка1", "Модель1", "Цвет1"));
-        carList.add(new Car("Марка2", "Модель2", "Цвет2"));
-        carList.add(new Car("Марка3", "Модель3", "Цвет3"));
-        carList.add(new Car("Марка4", "Модель4", "Цвет4"));
-        carList.add(new Car("Марка5", "Модель5", "Цвет5"));
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
 
     @GetMapping("/cars")
     public String getCars(@RequestParam(value = "count", required = false, defaultValue = "0") int count, Model model) {
-        if (count >= 5 || count <= 0) {
-            // Если count ≥ 5 или не указан, выводим весь список машин
-            model.addAttribute("carList", carList);
-        } else {
-            // Иначе выводим указанное число машин
-            model.addAttribute("carList", carList.subList(0, count));
-        }
+        List<Car> carList = carService.getCars(count);
+        model.addAttribute("carList", carList);
         return "cars";
     }
 }
---
+++ src/main/java/web/service/CarService.java
@@ -0,0 +1,29 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.models.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarService {
+    private List<Car> carList;
+
+    public CarService() {
+        carList = new ArrayList<>();
+        carList.add(new Car("Марка1", "Модель1", "Цвет1"));
+        carList.add(new Car("Марка2", "Модель2", "Цвет2"));
+        carList.add(new Car("Марка3", "Модель3", "Цвет3"));
+        carList.add(new Car("Марка4", "Модель4", "Цвет4"));
+        carList.add(new Car("Марка5", "Модель5", "Цвет5"));
+    }
+
+    public List<Car> getCars(int count) {
+        if (count >= 5 || count <= 0) {
+            return carList;
+        } else {
+            return carList.subList(0, count);
+        }
+    }
+}
