41b9e10f80303a0a5def1acc16fb2a550729cd86
Evgeny <pet9.wawa.95@gmail.com>
2023-07-26 19:35:50 +0300

    dependency on interfaces

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,22 +6,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.models.Car;
+import web.service.CarService;
 import web.service.CarServiceImpl;
 
 import java.util.List;
 
 @Controller
 public class CarController {
-    private final CarServiceImpl carServiceImpl;
+    private final CarService carService;
 
     @Autowired
-    public CarController(CarServiceImpl carServiceImpl) {
-        this.carServiceImpl = carServiceImpl;
+    public CarController(CarServiceImpl carService) {
+        this.carService = carService;
     }
 
     @GetMapping("/cars")
     public String getCars(@RequestParam(value = "count", required = false, defaultValue = "0") int count, Model model) {
-        List<Car> carList = carServiceImpl.getCars(count);
+        List<Car> carList = carService.getCars(count);
         model.addAttribute("carList", carList);
         return "cars";
     }
